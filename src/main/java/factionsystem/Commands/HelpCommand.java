1be61b02d18668f8379b2649af0b126565808a37
ART4IK09 <75059061+ART4IK09@users.noreply.github.com>
2020-11-30 19:30:08 +0300

    Update HelpCommand.java

--- src/main/java/factionsystem/Commands/HelpCommand.java
+++ src/main/java/factionsystem/Commands/HelpCommand.java
@@ -17,81 +17,81 @@ public class HelpCommand {
                 sendPageOne(sender);
             }
             if (args[1].equalsIgnoreCase("2")) {
-                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Medieval Factions Commands Page 2/6 == " + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf kick - Kick a player from your faction. " + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf transfer - Transfer ownership of your faction to another player.\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf disband - Disband your faction (must be owner)." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf declarewar - Declare war against another faction." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf makepeace - Send peace offer to another faction." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf claim - Claim land for your faction." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf unclaim - Unclaim land for your faction." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf unclaimall - Unclaim all land for your faction." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf checkclaim - Check if land is claimed." + "\n");
+                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + "\n == Фракции 2/6 == " + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf kick - Выгонить игрока из своей фракции. " + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf transfer - Передать право собственности на свою фракцию другому игроку.\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf disband - Распустить свою фракцию (создатель)." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf declarewar - Объявить войну другой фракции." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf makepeace - Отправить предложение мира другой фракции." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf claim - Заявить права на землю для своей фракции." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf unclaim - Отказатся от земли для своей фракции." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf unclaimall - Отказатся от всей земли для своей фракции." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf checkclaim - Проверить, заявлены ли права на землю." + "\n");
             }
             if (args[1].equalsIgnoreCase("3")) {
-                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Medieval Factions Commands Page 3/6 == " + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf autoclaim - Toggle auto claim, making land claiming easier." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf promote - Promote a player to officer status." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf demote - Demote an officer to member status." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf power - Check your power level." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf sethome - Set your faction home." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf home - Teleport to your faction home." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf who - View the faction info a specific player." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf ally - Attempt to ally with a faction." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf breakalliance - Break an alliance with a faction." + "\n");
+                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + "\n == Фракции 3/6 == " + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf autoclaim - Включить автоматическое требование, чтобы упростить получение земли." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf promote - Повысить игрока до статуса офицера." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf demote - Понизить офицера до статуса члена." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf power - Проверить свой уровень мощности." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf sethome - Установить свою фракцию домом." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf home - Телепортироватся в дом вашей фракции." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf who - Просмотр информации о фракции конкретного игрока." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf ally - Попытатся вступить в союз с фракцией." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf breakalliance - Разорвать союз с фракцией." + "\n");
             }
             if (args[1].equalsIgnoreCase("4")) {
-                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Medieval Factions Commands Page 4/6 == " + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf rename - Rename your faction" + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf lock - Lock a chest or door." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf unlock Unlock a chest or door." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf grantaccess - Grant someone access to a locked block.");
-                sender.sendMessage(ChatColor.AQUA + "/mf checkaccess - Check who has access to a locked block.");
-                sender.sendMessage(ChatColor.AQUA + "/mf revokeaccess - Revoke someone's access to a locked block.");
-                sender.sendMessage(ChatColor.AQUA + "/mf laws - View the laws of your faction.");
-                sender.sendMessage(ChatColor.AQUA + "/mf addlaw - Add a law to your faction.");
-                sender.sendMessage(ChatColor.AQUA + "/mf removelaw - Remove a law from your faction.");
+                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + "\n == Фракции 4/6 == " + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf rename - Переименовать свою фракцию" + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf lock - Заприте сундук или дверь." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf unlock Разблокировать сундук или дверь." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf grantaccess - Предоставить кому-либо доступ к заблокированному блоку.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf checkaccess - Проверить, у кого есть доступ к заблокированному блоку.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf revokeaccess - Отменить чей-либо доступ к заблокированному блоку.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf laws - Ознакомьтесь с законами вашей фракции.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf addlaw - Добавьте закон в свою фракцию.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf removelaw - Удалите закон из своей фракции.");
             }
             if (args[1].equalsIgnoreCase("5")) {
-                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Medieval Factions Commands Page 5/6 == " + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf editlaw - Edit an already existing law in your faction.");
-                sender.sendMessage(ChatColor.AQUA + "/mf chat - Toggle faction chat.");
-                sender.sendMessage(ChatColor.AQUA + "/mf vassalize - Offer to vassalize a faction.");
-                sender.sendMessage(ChatColor.AQUA + "/mf swearfealty - Swear fealty to a faction.");
-                sender.sendMessage(ChatColor.AQUA + "/mf declareindependence - Declare independence from your liege.");
-                sender.sendMessage(ChatColor.AQUA + "/mf grantindependence - Grant a vassal vaction independence.");
-		        sender.sendMessage(ChatColor.AQUA + "/mf gate create (<optional>name)");
-		        sender.sendMessage(ChatColor.AQUA + "/mf gate name (<optional>name)");
-		        sender.sendMessage(ChatColor.AQUA + "/mf gate list");
-		        sender.sendMessage(ChatColor.AQUA + "/mf gate remove");
-		        sender.sendMessage(ChatColor.AQUA + "/mf gate cancel");
+                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + "\n == Фракции 5/6 == " + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf editlaw - Отредактируйте уже существующий закон в вашей фракции.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf chat - Переключить чат фракции.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf vassalize - Предложить вассализировать фракцию.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf swearfealty - Поклянись в верности фракции.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf declareindependence - Объявите независимость от вашего сеньора.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf grantindependence - Предоставить вассальную независимость от действий.");
+		        sender.sendMessage(ChatColor.GOLD + "/mf gate create (<обязательно>имя) - Создать врата");
+		        sender.sendMessage(ChatColor.GOLD + "/mf gate name (<обязательно>имя) - Переименовать врата");
+		        sender.sendMessage(ChatColor.GOLD + "/mf gate list - Список врат в вашей фракции");
+		        sender.sendMessage(ChatColor.GOLD + "/mf gate remove - Убрать врата");
+		        sender.sendMessage(ChatColor.GOLD + "/mf gate cancel - Отмена действия(связаного с вратами)");
             }
             if (args[1].equalsIgnoreCase("6")) {
-                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Medieval Factions Commands Page 6/6 == " + "\n");
-		        sender.sendMessage(ChatColor.AQUA + "/mf duel challenge (player) (<optional>time limit in seconds)");
-		        sender.sendMessage(ChatColor.AQUA + "/mf duel accept (<optional>player)");
-		        sender.sendMessage(ChatColor.AQUA + "/mf duel cancel");
-                sender.sendMessage(ChatColor.AQUA + "/mf bypass - Bypass protections.");
-                sender.sendMessage(ChatColor.AQUA + "/mf config show - Show config values.");
-                sender.sendMessage(ChatColor.AQUA + "/mf config set (option) (value) - Set a config value.");
-                sender.sendMessage(ChatColor.AQUA + "/mf force - Force the plugin to perform certain actions." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf version - Check plugin version." + "\n");
-                sender.sendMessage(ChatColor.AQUA + "/mf resetpowerlevels - Reset player power records and faction cumulative power levels." + "\n");
+                sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Фракции 6/6 == " + "\n");
+		        sender.sendMessage(ChatColor.GOLD + "/mf duel challenge (игрок) (время) - Начать дуель");
+		        sender.sendMessage(ChatColor.GOLD + "/mf duel accept (игрок) - Принять приглашение в дуель");
+		        sender.sendMessage(ChatColor.GOLD + "/mf duel cancel - Отмена текущего действия");
+                sender.sendMessage(ChatColor.YELLOW + "/mf bypass - Обход защиты.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf config show - Показать значения конфигурации.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf config set (функция) (значение) - Установите значение конфигурации.");
+                sender.sendMessage(ChatColor.YELLOW + "/mf force - Заставить плагин выполнять определенные действия." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf version - Проверить версию плагина." + "\n");
+                sender.sendMessage(ChatColor.YELLOW + "/mf resetpowerlevels - Сбросить рекорды силы игроков и уровни совокупной силы фракций." + "\n");
             }
         }
         return true;
     }
 
     static void sendPageOne(CommandSender sender) {
-        sender.sendMessage(ChatColor.BOLD + "" + ChatColor.AQUA + "\n == Medieval Factions Commands Page 1/6 == " + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf help # - Show lists of useful commands." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf list - List all factions on the server." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf info - See your faction or another faction's information." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf members - List the members of your faction or another faction." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf join - Join a faction if you've been invited." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf leave - Leave your current faction." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf create - Create a new faction." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf invite - Invite a player to your faction." + "\n");
-        sender.sendMessage(ChatColor.AQUA + "/mf desc - Set your faction description." + "\n");
+        sender.sendMessage(ChatColor.BOLD + "" + ChatColor.GOLD + "\n == Фракции 1/6 == " + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf help # - Список всех команд." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf list - Список фракций на сервере." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf info - Посмотреть информацию про свою или чужую фракцию." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf members - Список членов вашей фракции или другой фракции." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf join - Присоединяйтесь к фракции, если вас пригласили." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf leave - Оставьте свою фракцию." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf create - Создать новую фракцию." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf invite - Пригласите игрока в свою фракцию." + "\n");
+        sender.sendMessage(ChatColor.YELLOW + "/mf desc - Установите описание вашей фракции." + "\n");
     }
 }
